ceabb783821761ec4c4673fe6e6f551ff539108c
Tian Welgemoed <tian.welgemoed@gmail.com>
2023-07-15 02:17:05 +1200

    update script to use github automation
    once per day at 8pm

--- main.py
+++ main.py
@@ -1,20 +1,14 @@
 import discord
-import asyncio
 from datetime import datetime
+import os
 
-# Read the token
-with open('token.txt', 'r') as token_file:
-    TOKEN = token_file.read().strip()
-# Read the channel ID
-with open('channel_id.txt', 'r') as channel_id_file:
-    CHANNEL_ID = int(channel_id_file.read().strip())
-# Read the questions
+TOKEN = os.getenv('TOKEN')
+CHANNEL_ID = int(os.getenv('CHANNEL_ID'))
+
 with open('questions.txt', 'r') as file:
     questions = file.readlines()
-
 # Remove any trailing newline characters and whitespace
 questions = [question.strip() for question in questions]
-# Display the number of retrieved questions
 num_questions = len(questions)
 print(f"Retrieved {num_questions} questions.")
 
@@ -43,15 +37,6 @@ async def on_ready():
 async def send_daily_question():
     await client.wait_until_ready()
     channel = client.get_channel(CHANNEL_ID)
-    # await channel.send(content=get_question_formatted()) # force run once
-
-    while not client.is_closed():
-        # Check if it's the next day and send the question
-        current_time = datetime.now().strftime('%H:%M:%S')
-        if current_time == '20:00:00':
-            await channel.send(content=get_question_formatted())
-
-        # Sleep for 1 second before checking the time again
-        await asyncio.sleep(1)
+    await channel.send(content=get_question_formatted()) # force run once
 
 client.run(TOKEN)
